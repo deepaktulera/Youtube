c6149dc09cbd77e71434d16d377bfb95652f7b4b
deepaktulera <deepakstuleraoo3@gmail.com>
2026-06-22 16:42:01 +0530

    Implement header section

--- Youtube-Frontend/src/App.jsx
+++ Youtube-Frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import AppRoutes from './routes/AppRoutes';
 
 const App = () => {
   return (
-    <div>
-      <h1>hello</h1>
+    <div className='p-2 w-screen h-screen'>
+      <AppRoutes />
     </div>
   );
 }
