850125ac23cfd3cce403ad9be3cb500b2eb62e14
deepaktulera <deepakstuleraoo3@gmail.com>
2026-06-23 15:00:29 +0530

    Implement sidebar

---
+++ Youtube-Frontend/src/components/Sidebar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { House ,UserRound} from 'lucide-react';
+import HomeIcon from '../assets/icons/home-material.svg'
+import shortsIcon from "../assets/icons/youtube_shorts.svg";
+import subscribeIcon from '../assets/icons/subscriptions.svg';
+import userIcon from '../assets/icons/user.svg' 
+
+const Sidebar = () => {
+  return (
+    <aside className='flex flex-col items-start justify-center'>
+        <button className='flex w-16 h-18 gap-1 flex-col justify-center p-1 items-center hover:bottom-1 hover:bg-gray-200 rounded-xl'><img src={HomeIcon} alt="" /><span className='text-xs'>Home</span></button>
+        <button className='flex w-16 h-18 gap-1 flex-col justify-center p-1 items-center hover:bottom-1 hover:bg-gray-200 rounded-xl'><img src={shortsIcon} alt="shorts"/><span className='text-xs'>Shorts</span></button>
+        <button className='flex w-16 h-18 gap-1 flex-col justify-center p-1 items-center hover:bottom-1 hover:bg-gray-200 rounded-xl'><img src={subscribeIcon} alt=""/><span className='text-xs'>Subscription</span></button>
+        <button className='flex w-16 h-18 gap-1 flex-col justify-center p-1 items-center hover:bottom-1 hover:bg-gray-200 rounded-xl'><img src={userIcon} alt="" /><span className='text-xs'>You</span></button>
+    </aside>
+  );
+}
+
+export default Sidebar;
--- Youtube-Frontend/src/pages/Home.jsx
+++ Youtube-Frontend/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
+import Sidebar from '../components/Sidebar';
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <section className='grid grid-cols-[1fr_9fr]'>
+      <Sidebar />
+      </section>
     </div>
   );
 }
