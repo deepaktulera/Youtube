826d04febde472cc7922332983d4c3ff8941dfce
deepaktulera <deepakstuleraoo3@gmail.com>
2026-06-26 22:29:44 +0530

    Implement Video Schema

---
+++ Youtube-Backend/src/models/Video.js
@@ -0,0 +1,82 @@
+import mongoose from "mongoose";
+
+const VideoSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    thumbnailUrl: {
+      type: String,
+      required: true,
+    },
+
+    videoUrl: {
+      type: String,
+      required: true,
+    },
+
+    category: {
+      type: String,
+      required: true,
+      enum: [
+        "All",
+        "Music",
+        "Gaming",
+        "News",
+        "Sports",
+        "Education",
+        "Programming",
+        "Entertainment",
+      ],
+    },
+
+    channel: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Channel",
+      required: true,
+    },
+
+    uploader: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    views: {
+      type: Number,
+      default: 0,
+    },
+
+    likes: {
+      type: Number,
+      default: 0,
+    },
+
+    dislikes: {
+      type: Number,
+      default: 0,
+    },
+
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Video = mongoose.model("Video", VideoSchema);
+
+export default Video;
