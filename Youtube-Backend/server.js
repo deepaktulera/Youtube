ac8a4659b83506442d4c5f0e4f9d073edca1228a
deepaktulera <deepakstuleraoo3@gmail.com>
2026-06-27 11:25:00 +0530

    Implement auth route

--- Youtube-Backend/server.js
+++ Youtube-Backend/server.js
@@ -1,9 +1,22 @@
 import express from 'express';
+import cors from 'cors';
+import connectDatabase from './src/config/databse.js';
+import dotenv from "dotenv";
+import route from './src/routes/auth.routes.js';
 
 const app = express();
 
-const PORT = 3000 || process.env.PORT 
+dotenv.config();
 
-app.listen(PORT , ()=> {
+connectDatabase();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", route);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
     console.log("Server Started");
 });
